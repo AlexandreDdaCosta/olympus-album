a7701f069b843104fe422787bf3f0c23b1c11800
Alexandre da Costa <alexandre.dias.dacosta@gmail.com>
2023-05-20 01:55:20 -0400

    Some missing check-ins.

--- album/views.py
+++ album/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class SpaceshipEarth(TemplateView):
+class AlbumHome(TemplateView):
 
     def get(self, errors=False, *args, **kwargs):
         context = {}
-        return render(self.request, 'spaceshipearth.html', context)
+        return render(self.request, 'album_home.html', context)
